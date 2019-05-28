585e22fb5544bbd02b4f6faf9466a2c49e71c60a
Angeliza Medina Quintana <5884603@rts-365.dk>
2019-05-28 12:20:32 +0200

    Fix routes and links

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,23 @@
 module.exports = (app) => {
 
-   app.get('/', (req, res, next) => {
+   app.get('/home', (req, res, next) => {
       res.render('home');
+   });
+
+   app.get('/about', (req, res, next) => {
+      res.render('about');
+   });
+
+   app.get('/catagories-post', (req, res, next) => {
+      res.render('catagories-post');
+   });
+
+   app.get('/single-post', (req, res, next) => {
+      res.render('single-post');
+   });
+
+   app.get('/contact', (req, res, next) => {
+      res.render('contact');
    });
 
 };
