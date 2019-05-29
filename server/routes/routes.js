ea0b158fa219fb167b1f850473ce04e8a642f69b
Angeliza Medina Quintana <5884603@rts-365.dk>
2019-05-29 12:39:09 +0200

    Render() objects + Create html elements to 'Latest News' section on the Home Page

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,16 +1,41 @@
 module.exports = (app) => {
 
+
    app.get('/', (req, res, next) => {
-		 
-      res.render('home');
+
+		let popularNews = [
+			{
+				'category': 'FINANACE',
+				'title': 'Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac...',
+				'img': 'img/bg-img/12.jpg'
+			},
+			{
+				'category': 'FINANACE',
+				'title': 'Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac...',
+				'img': 'img/bg-img/13.jpg'
+			},
+			{
+				'category': 'FINANACE',
+				'title': 'Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac...',
+				'img': 'img/bg-img/14.jpg'
+			},
+			{
+				'category': 'FINANACE',
+				'title': 'Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac...',
+				'img': 'img/bg-img/15.jpg'
+			}
+		]
+
+      res.render('home', {popularNews});
    });
 
    app.get('/about', (req, res, next) => {
+
       res.render('about');
    });
 
-   app.get('/catagories-post', (req, res, next) => {
-      res.render('catagories-post');
+   app.get('/categories-post', (req, res, next) => {
+      res.render('categories-post');
    });
 
    app.get('/single-post', (req, res, next) => {
