98e271809d271c7abe1069e7a0ea607da347fff6
Angeliza Medina Quintana <5884603@rts-365.dk>
2019-06-11 07:52:24 +0200

    Make a new database

--- database/the_awesome_news_page.sql
+++ database/the_awesome_news_page.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 07, 2019 at 01:06 PM
+-- Generation Time: Jun 11, 2019 at 07:51 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -25,6 +25,47 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `articles`
+--
+
+CREATE TABLE `articles` (
+  `article_id` int(11) NOT NULL,
+  `article_title` varchar(300) NOT NULL,
+  `article` varchar(5000) NOT NULL,
+  `FK_article_img` int(11) NOT NULL,
+  `FK_article_category` int(11) NOT NULL,
+  `article_date_time` varchar(20) NOT NULL,
+  `FK_author_name` int(11) NOT NULL,
+  `FK_author_img` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article_imgs`
+--
+
+CREATE TABLE `article_imgs` (
+  `article_img_id` int(11) NOT NULL,
+  `article_img` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors`
+--
+
+CREATE TABLE `authors` (
+  `author_id` int(11) NOT NULL,
+  `author_name` varchar(100) NOT NULL,
+  `author_email` varchar(300) NOT NULL,
+  `author_img` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `categories`
 --
 
@@ -41,10 +82,10 @@ CREATE TABLE `categories` (
 
 CREATE TABLE `comments` (
   `comment_id` int(11) NOT NULL,
-  `fk_user` int(11) NOT NULL,
-  `fk_user_img` int(11) NOT NULL,
+  `FK_user` int(11) NOT NULL,
+  `FK_user_img` int(11) NOT NULL,
   `comment` varchar(1000) NOT NULL,
-  `comment_post_date` date NOT NULL
+  `comment_date_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -55,16 +96,52 @@ CREATE TABLE `comments` (
 
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(300) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_email` varchar(300) NOT NULL,
   `password` varchar(100) NOT NULL,
   `user_img` varchar(100) NOT NULL,
+  `birthday` date NOT NULL,
   `account_creation_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `videos`
+--
+
+CREATE TABLE `videos` (
+  `video_id` int(11) NOT NULL,
+  `video_title` varchar(100) NOT NULL,
+  `video_src` varchar(300) NOT NULL,
+  `video_img` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`article_id`),
+  ADD KEY `FK_author_name` (`FK_author_name`),
+  ADD KEY `FK_author_img` (`FK_author_img`),
+  ADD KEY `article_img` (`FK_article_img`),
+  ADD KEY `FK_article_category` (`FK_article_category`);
+
+--
+-- Indexes for table `article_imgs`
+--
+ALTER TABLE `article_imgs`
+  ADD PRIMARY KEY (`article_img_id`);
+
+--
+-- Indexes for table `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`author_id`);
 
 --
 -- Indexes for table `categories`
@@ -77,8 +154,8 @@ ALTER TABLE `categories`
 --
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`comment_id`),
-  ADD KEY `fk_user` (`fk_user`),
-  ADD KEY `fk_user_img` (`fk_user_img`);
+  ADD KEY `fk_user` (`FK_user`),
+  ADD KEY `fk_user_img` (`FK_user_img`);
 
 --
 -- Indexes for table `users`
@@ -87,8 +164,32 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
 --
+-- Indexes for table `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`video_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `article_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `article_imgs`
+--
+ALTER TABLE `article_imgs`
+  MODIFY `article_img_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `author_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -107,6 +208,23 @@ ALTER TABLE `comments`
 --
 ALTER TABLE `users`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `videos`
+--
+ALTER TABLE `videos`
+  MODIFY `video_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`fk_user`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`fk_user_img`) REFERENCES `users` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
