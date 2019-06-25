0e60499dd2cabffdaba5510bb09b964d39b61bb2
Angeliza Medina Quintana <5884603@rts-365.dk>
2019-06-25 13:22:50 +0200

    Delete some routes. Focus on one at a time for now

--- server/routes/admin_categories.js
+++ server/routes/admin_categories.js
@@ -8,54 +8,31 @@ module.exports = app => {
 
 		let db = await mysql.connect();
 
-		res.render('admin_categories');
+		let categories = await getCategories();
+
+		res.render('admin_categories', {
+			'categories': categories
+		});
 
 		db.end();
 
    });
    
-
-   app.post('/admin/categories', async (req, res, next) => {
-
-		let db = await mysql.connect();
-
-		res.render('admin_categories');
-
-		db.end();
-
-   });
-   
-
-   app.get('/admin/categories/edit/:category_id', async (req, res, next) => {
-
-		let db = await mysql.connect();
-
-		res.render('admin_categories');
-
-      db.end();
-
-   });
-   
-
-   app.post('/admin/categories/edit/:category_id', async (req, res, next) => {
-
-		let db = await mysql.connect();
-
-		res.render('admin_categories');
-
-		db.end();
-
-   });
-   
-
-   app.get('/admin/categories/delete/:category_id', async (req, res, next) => {
-
-		let db = await mysql.connect();
-
-		res.render('admin_categories');
-
-		db.end();
-
-	});
-
 }; // module.export end
+
+
+async function getCategories(){
+	let db = await mysql.connect();
+	let [categories] = await db.execute(`
+		SELECT
+		category_id,
+		category
+
+		FROM categories
+
+		ORDER BY category
+
+		`);
+	db.end();
+	return categories;
+}
